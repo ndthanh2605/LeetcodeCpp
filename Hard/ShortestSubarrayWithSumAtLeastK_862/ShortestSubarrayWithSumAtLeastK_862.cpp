44253d2619e0ec8540594f0e1bac49c67dfa1ce1
ThanhNgx <ndthanh2605@gmail.com>
2024-11-17 14:15:19 +0700

    (Hard) Shortest subarray with sum at least k

---
+++ Hard/ShortestSubarrayWithSumAtLeastK_862/ShortestSubarrayWithSumAtLeastK_862.cpp
@@ -0,0 +1,25 @@
+// ShortestSubarrayWithSumAtLeastK_862.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int minLength = -1;
+
+    int shortestSubarray(vector<int>& nums, int k) {
+
+    }
+};
+
+int main()
+{
+    vector<int> nums = { 84,-37,32,40,95 };
+    Solution sln;
+    cout << sln.shortestSubarray(nums, 167) << endl;
+
+    return 0;
+}
